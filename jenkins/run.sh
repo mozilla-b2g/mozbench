cdb3b72f4cf72360ff4552ac7b66aa5fc3274f6b
Ricardo Castro <mcc.ricardo@gmail.com>
2014-08-30 00:03:07 +0100

    Updated Jenkins script for OS X.

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -5,4 +5,9 @@ python setup.py install
 FIREFOX_URL=http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central
 JENKINS_DIR=`dirname $0`
 VERSION=`python $JENKINS_DIR/firefox_version.py $FIREFOX_URL`
-python -m mozbench.mozbench --firefox-url $FIREFOX_URL/$VERSION.linux-x86_64.tar.bz2 --chrome-path google-chrome --log-mach=- --post-results
+if [[ `uname` == 'Linux' ]]; then
+   python -m mozbench.mozbench --firefox-url $FIREFOX_URL/$VERSION.linux-x86_64.tar.bz2 --chrome-path google-chrome --log-mach=- --post-results
+elif [[ `uname` == 'Darwin' ]]; then
+   python -m mozbench.mozbench --firefox-url $FIREFOX_URL/$VERSION.mac.dmg --chrome-path "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --log-mach=- --post-results
+fi
+
