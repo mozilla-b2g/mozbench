07c568c6213db3aaec1257283c47de442b5d842e
Ricardo Castro <mcc.ricardo@gmail.com>
2014-09-18 22:26:55 +0100

    Initial setup for args parsing and logging. Checking if any device is connected.

--- mozbench/mozbench-android.py
+++ mozbench/mozbench-android.py
@@ -2,4 +2,28 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import argparse
+import sys
+import logging
 import mozdevice
+from mozlog.structured import commandline
+
+
+def cli(args):
+
+    parser = argparse.ArgumentParser()
+    commandline.add_logging_group(parser)
+    args = parser.parse_args(args)
+
+    logging.basicConfig()
+    logger = commandline.setup_logging('mozbench-android', vars(args), {})
+
+    adb_host = mozdevice.ADBHost()
+    devices = adb_host.devices()
+    if not devices:
+        logger.error('No devices found')
+        return 1
+
+
+if __name__ == '__main__':
+    exit(cli(sys.argv[1:]))
