517e383d5b766a4987d47a841c8addd1b3bc4cb3
Ricardo Castro <mcc.ricardo@gmail.com>
2014-11-03 22:44:28 +0000

    Bug 1079438 - Add Unity's WebGLBenchmark to mozbench

---
+++ mozbench/static/Unity-WebGLBenchmark/Data/mozbench.js
@@ -0,0 +1,45 @@
+
+var benchmarks = ['Mandelbrot GPU', 'Mandelbrot Script',
+  'Instantiate & Destroy', 'CryptoHash Script', 'Animation & Skinning',
+  'Asteroid Field', 'Particles', 'Physics Meshes', 'Physics Cubes',
+  'Physics Spheres', '2D Physics Spheres', '2D Physics Boxes', 'AI Agents'];
+
+var results = [];
+
+function processText(text) {
+  var test = text.split(':');            
+  
+  if (test[0] === 'Overall') {
+    // Add Overall result to results
+    results.push({
+      benchmark: 'Overall',
+      result: test[1]
+    });
+
+    // Post results back to Mozbench
+    postResults();
+    return;
+  }
+
+  // We haven't reach the end. Let's check if we have a possible result.
+  for (var i = 0 ; i < benchmarks.length; i++) {
+    if (test[0] === benchmarks[i]) {
+      // Add benchmark result to results
+      results.push({
+        benchmark: test[0],
+        result: test[1]
+      });
+      break;        
+    }
+  }
+}
+
+function postResults() {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", "/results", true);
+  xmlHttp.setRequestHeader("Content-type", 
+                            "application/x-www-form-urlencoded");
+  // We need to use encodeURIComponent because of the '&' in benchmark names.
+  xmlHttp.send("results=" + encodeURIComponent(JSON.stringify(results)));        
+}
+
