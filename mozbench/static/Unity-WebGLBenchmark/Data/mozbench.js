d8530f1879af1b24898f55a88bd810b16f5b4548
Ricardo Castro <mcc.ricardo@gmail.com>
2014-11-20 22:52:57 +0000

    Removed unnecessary function.

--- mozbench/static/Unity-WebGLBenchmark/Data/mozbench.js
+++ mozbench/static/Unity-WebGLBenchmark/Data/mozbench.js
@@ -56,36 +56,3 @@ function startBenchmark() {
   var event = new Event("touchstart");
   canvas.dispatchEvent(event);
 }
-
-function handleEvent(e){
- var evt = e ? e:window.event;
- var clickX=0, clickY=0;
-
- if ((evt.clientX || evt.clientY) &&
-     document.body &&
-     document.body.scrollLeft!=null) {
-  clickX = evt.clientX + document.body.scrollLeft;
-  clickY = evt.clientY + document.body.scrollTop;
- }
- if ((evt.clientX || evt.clientY) &&
-     document.compatMode=='CSS1Compat' && 
-     document.documentElement && 
-     document.documentElement.scrollLeft!=null) {
-  clickX = evt.clientX + document.documentElement.scrollLeft;
-  clickY = evt.clientY + document.documentElement.scrollTop;
- }
- if (evt.pageX || evt.pageY) {
-  clickX = evt.pageX;
-  clickY = evt.pageY;
- }
-
- alert (evt.type.toUpperCase() + ' mouse event:'
-  +'\n pageX = ' + clickX
-  +'\n pageY = ' + clickY 
-  +'\n clientX = ' + evt.clientX
-  +'\n clientY = '  + evt.clientY 
-  +'\n screenX = ' + evt.screenX 
-  +'\n screenY = ' + evt.screenY
- )
- return false;
-}
