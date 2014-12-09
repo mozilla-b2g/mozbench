d042b8c09bf49dc7c748180906b6d1b4e41d4144
Dan Minor <dminor@mozilla.com>
2014-12-09 13:28:22 -0500

    Fix ftp path for Android to account for apk split

--- jenkins/run-android.sh
+++ jenkins/run-android.sh
@@ -5,7 +5,7 @@ rm -rf venv
 virtualenv venv
 . venv/bin/activate
 python setup.py install
-FIREFOX_URL=http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android
+FIREFOX_URL=http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android-api-10
 JENKINS_DIR=`dirname $0`
 VERSION=`python $JENKINS_DIR/firefox_version.py $FIREFOX_URL`
 python -m mozbench.mozbench --firefox-url $FIREFOX_URL/$VERSION.android-arm.apk --use-android --log-mach=- --post-results
