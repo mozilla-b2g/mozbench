a20046d9443f40ac41e9ce25c3dbe7eb9de17a54
Ricardo Castro <mcc.ricardo@gmail.com>
2014-12-15 12:43:19 +0000

    Updated jenkins scripts

--- jenkins/run-android.sh
+++ jenkins/run-android.sh
@@ -8,4 +8,9 @@ python setup.py install
 FIREFOX_URL=http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android-api-10
 JENKINS_DIR=`dirname $0`
 VERSION=`python $JENKINS_DIR/firefox_version.py $FIREFOX_URL`
+
+# Extract WebGLBenchmark files
+tar xvzf mozbench/static/Unity-WebGLBenchmark/Data/WebGLBenchmarks.data.tar.gz -C mozbench/static/Unity-WebGLBenchmark/Data/
+tar xvzf mozbench/static/Unity-WebGLBenchmark/Data/WebGLBenchmarks.js.tar.gz -C mozbench/static/Unity-WebGLBenchmark/Data/
+
 python -m mozbench.mozbench --firefox-url $FIREFOX_URL/$VERSION.android-arm.apk --use-android --log-mach=- --post-results
